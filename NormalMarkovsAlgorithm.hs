6b1591708f654649416888c247dd593990f72880
Alexey <mrfaithya@gmail.com>
2013-09-19 21:59:44 +0400

    Update NormalMarkovsAlgorithm.hs

--- NormalMarkovsAlgorithm.hs
+++ NormalMarkovsAlgorithm.hs
@@ -1,3 +1,4 @@
+-- find index of substring in string
 findIndex :: String -> String -> Int
 findIndex str sub 
     | str == sub = 1
@@ -5,31 +6,39 @@ findIndex str sub
     | take (length sub) str == sub = 1
     | otherwise = if idx > 0 then 1 + idx else -1
         where idx = findIndex (drop 1 str) sub 
-
-find :: String -> String -> Maybe (String,String)
-find str sub = let idx = findIndex str sub in
+-- find substring in string and split it (left,sub,right) -> (left,right)
+findAndSplit :: String -> String -> Maybe (String,String)
+findAndSplit str sub = let idx = findIndex str sub in
     if idx <= 0 then Nothing
                 else Just (take (idx - 1) str,drop (idx + length sub - 1) str)
-
-doRule :: [(String,String,Bool)] -> String -> Maybe (String,Bool)
-doRule [] _ = Nothing
-doRule rules str = case find str sub of 
-    Nothing -> doRule (drop 1 rules) str
+-- find first matched rule and process it.
+processRules :: [(String,String,Bool)] -> String -> Maybe (String,Bool)
+processRules [] _ = Nothing
+processRules rules str = case findAndSplit str sub of 
+    Nothing -> processRules (drop 1 rules) str
     Just (left,right) -> Just (left ++ pattern ++ right,not next)
     where (sub,pattern,next) = head rules 
-
-runNMA :: [(String,String,Bool)] -> String -> String
-runNMA rules str = case doRule rules str of
-    Nothing -> str
-    Just (newstr,next) -> if next then runNMA rules newstr 
-                                  else newstr
-
-(~>),(!~>) :: String -> String -> (String,String,Bool)
+-- main function of NMA
+logAndRun :: (t -> String -> IO b) -> t -> String -> IO b
+logAndRun f r n = do putStrLn n
+                     f r n
+runNMA :: [(String,String,Bool)] -> String -> IO String
+runNMA rules str = case processRules rules str of
+    Nothing -> return str
+    Just (newstr,next) -> if next then logAndRun runNMA rules newstr 
+                                  else return newstr
+-- sugar
+(~>),(|~>) :: String -> String -> (String,String,Bool)
 (~>) str1 str2 = (str1,str2,False)
-(!~>) str1 str2 = (str1,str2,True)
-
+(|~>) str1 str2 = (str1,str2,True)
 --example
 main :: IO()
-main = putStrLn $ runNMA rules "aabcbabcc" -- res = "123ebqwec"
-    where rules = [ "abc" ~> "qwe",
-                    "qw" !~> "123"]
+main = do s <- runNMA rules "||||||||||||||||||||||||||||||" -- res = "|||*|||"
+          putStrLn s
+    where rules = [ "13" ~>  "3|",
+                    "42" ~>  "|4",
+                    "3"  ~>  "|",
+                    "4"  |~> "|",
+                    "||" ~>  "12",
+                    "21" ~>  "12",
+                    "12" ~>  "3*4"]
